243a3e07dda8aa7b6ee417aefb4f0a1d64b9911f
Kristen Waterford <123219300+KWATER2352@users.noreply.github.com>
2025-07-02 23:56:24 -0700

    test.asm

--- test.asm
+++ test.asm
@@ -1,9 +1,9 @@
 section .data
-    value1 db 0xF  ; example value
-    value2 db 0x1  ; example value to test with
+    value1 db 0xF 
+    value2 db 0x1 
 
 section .bss
-    ; no uninitialized data needed for this task
+    
 
 section .text
 global _start
@@ -12,18 +12,17 @@ _start:
     mov al, [value1]
     mov bl, [value2]
 
-    ; Perform TEST operation
     test al, bl
 
-    ; Setup exit based on ZF flag set by TEST
-    jz exit_with_zero   ; Jump if zero flag is set (result of AND was zero)
+    jz exit_with_zero   
 
 continue_program:
-    ; If TEST result is non-zero, continue the program (dummy operation)
-    nop  ; No operation, just a placeholder
+ 
+    nop  
 
 exit_with_zero:
-    ; Exit the program
+
     mov eax, 1
     mov ebx, 0
     int 0x80
+
